38d0f7c5abdc3bcc8ec97ba70e47029b11562e1e
like <like@adtech.com.cn>
2019-05-24 15:47:28 +0800

    升级

--- livedatabus/src/main/java/com/like/livedatabus/BusLiveData.kt
+++ livedatabus/src/main/java/com/like/livedatabus/BusLiveData.kt
@@ -5,7 +5,7 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.Observer
 
 /**
- * 问题：在使用这个LiveDataBus的过程中，订阅者会收到订阅之前发布的消息。
+ * 解决问题：在使用这个LiveDataBus的过程中，订阅者会收到订阅之前发布的消息。
  *
  * 原因：对于LiveData，其初始的version是-1，当我们调用了其setValue或者postValue，其vesion会+1；
  * 对于每一个观察者的封装ObserverWrapper，其初始version也为-1，也就是说，每一个新注册的观察者，其version为-1；
@@ -13,7 +13,8 @@ import android.arch.lifecycle.Observer
  * LiveData就会强制把当前value推送给Observer。
  *
  * 解决办法：只需要在注册一个新的订阅者的时候把ObserverWrapper的version设置成跟LiveData的version一致即可。
- * ①、看看LiveData的observe方法，他会创建一个LifecycleBoundObserver，LifecycleBoundObserver是ObserverWrapper的派生类。
+ * ①、采用observe注册时：
+ * 会创建一个LifecycleBoundObserver，LifecycleBoundObserver是ObserverWrapper的派生类。
  * 然后会把这个LifecycleBoundObserver放入一个私有Map容器mObservers中。
  * 无论ObserverWrapper还是LifecycleBoundObserver都是私有的或者包可见的，
  * 所以无法通过继承的方式更改LifecycleBoundObserver的version。
@@ -21,13 +22,13 @@ import android.arch.lifecycle.Observer
  * 答案是肯定的，通过查看SafeIterableMap的源码我们发现有一个protected的get方法。
  * 因此，在调用observe的时候，我们可以通过反射拿到LifecycleBoundObserver，
  * 再把LifecycleBoundObserver的version设置成和LiveData一致即可。
- * ②、对于非生命周期感知的observeForever方法来说，实现的思路是一致的，但是具体的实现略有不同。
- * observeForever的时候，生成的wrapper不是LifecycleBoundObserver，而是AlwaysActiveObserver，
+ * ②、采用observeForever注册时：
+ * 生成的wrapper不是LifecycleBoundObserver，而是AlwaysActiveObserver，
  * 而且我们也没有机会在observeForever调用完成之后再去更改AlwaysActiveObserver的version，
  * 因为在observeForever方法体内，最后调用了wrapper.activeStateChanged(true)。
  * 既然是在调用内回调的，那么我们可以写一个ObserverWrapper，把真正的回调给包装起来。
  * 把ObserverWrapper传给observeForever，那么在回调的时候我们去检查调用栈，
- * 如果回调是observeForever方法引起的，那么就不回调真正的订阅者。
+ * 如果回调是因为调用observeForever方法引起的，那么就不回调真正的订阅者。
  */
 class BusLiveData<T> : MutableLiveData<T>() {
     /**
