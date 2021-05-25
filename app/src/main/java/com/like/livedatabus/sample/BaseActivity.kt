1003b98dadb67ccfdf1604346c8c349435f5c7b6
like5188 <260864866@qq.com>
2021-05-25 16:20:46 +0800

    完善

--- app/src/main/java/com/like/livedatabus/sample/BaseActivity.kt
+++ app/src/main/java/com/like/livedatabus/sample/BaseActivity.kt
@@ -1,10 +1,8 @@
 package com.like.livedatabus.sample
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.like.livedatabus.LiveDataBus
-import com.like.livedatabus_annotations.BusObserver
 
 open class BaseActivity : AppCompatActivity() {
 
